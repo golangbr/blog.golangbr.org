ce44655d4d6675166fe703d0c11097695d6ccbfb
maiconio <maiconscosta@gmail.com>
2014-05-06 01:05:31 -0300

    Geração do arquivo markdown

--- script/main.go
+++ script/main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"encoding/xml"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"net/http"
+	"net/url"
 	"os"
 )
 
@@ -13,8 +15,10 @@ type XMLFeed struct {
 }
 
 type Entrada struct {
-	Id string `xml:"id"`
-	Conteudo string `xml:"content"`
+	Id             string `xml:"id"`
+	Conteudo       string `xml:"content"`
+	Titulo         string `xml:"title"`
+	DataPublicacao string `xml:"published"`
 }
 
 func (x XMLFeed) String() string {
@@ -26,6 +30,12 @@ func (e Entrada) String() string {
 }
 
 func main() {
+        //1- [TODO] olha arquivo texto no repositório do blog com lista de blogs
+        //2- [TODO] iniciar uma goroutine para olhar se o blog possui feeds
+        //3- [OK]   caso possua, parseia entradas e olha se a última possui a tag golang (apenas a última para simplificar)
+        //4- [OK]   caso possua a tag transforma em markdown com o post e informações e "commita" no diretório de posts
+        //5- [TODO] commit dispara o deploy automatizado que atualiza o blog
+
 	//response, err := http.Get("http://maiconio.blogspot.com/feeds/posts/default")
 	resp, err := http.Get("http://127.0.0.1/teste/default")
 
@@ -43,11 +53,23 @@ func main() {
 		var x XMLFeed
 		xml.Unmarshal(conteudoXML, &x)
 
-                ultimaEntrada := x.Entradas[len(x.Entradas)-1:]
+		ultimaEntrada := x.Entradas[len(x.Entradas)-1]
 
-		fmt.Printf("\t%s\n", ultimaEntrada)
+		nomeArquivo := ultimaEntrada.DataPublicacao[0:10] + "-" + url.QueryEscape(ultimaEntrada.Titulo) + ".md"
+		postMarkdown, err := os.Create("../_posts/"+nomeArquivo)
+		if err != nil {
+			fmt.Printf("%s", err)
+			os.Exit(1)
+		} else {
 
-                //TODO: salva arquivo markdown
+			io.WriteString(postMarkdown, "---\n")
+			io.WriteString(postMarkdown, "layout: default\n")
+			io.WriteString(postMarkdown, "title: "+ultimaEntrada.Titulo+"\n")
+			io.WriteString(postMarkdown, "---\n")
+			io.WriteString(postMarkdown, ultimaEntrada.Conteudo)
+			postMarkdown.Close()
+		}
+
 	}
 
 }
